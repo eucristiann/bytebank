4322edb9a1d75f0badaa576b4e3bd39c1816eb2b
Cristian Nunes <cristian.nv@hotmail.com>
2024-02-29 20:12:53 -0300

    att

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 class Cliente {
     nome;
     cpf;
+    
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if(this.saldo >= valor){
+            this.saldo -= valor
+        }
+    }
+
+    depositar(valor) {
+        if(valor > 0){
+            this.saldo += valor
+        }
+    }
 }
 
 const cliente1 = new Cliente()
-const cliente2 = new Cliente()
-
 cliente1.nome = 'Ricardo'
 cliente1.cpf = 11122233309
-cliente1.agencia = 1001
-cliente1.saldo = 0
 
+const cliente2 = new Cliente()
 cliente2.nome = 'Alice'
 cliente2.cpf = 88822233309
-cliente2.agencia = 1001
-cliente2.saldo = 0
+
+const contaCorrenteRicardo = new ContaCorrente()
+contaCorrenteRicardo.saldo = 0
+contaCorrenteRicardo.agencia = 1001
+contaCorrenteRicardo.saldo = 100
+contaCorrenteRicardo.sacar(50)
+contaCorrenteRicardo.depositar(100)
