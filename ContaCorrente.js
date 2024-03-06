fb1c7c9899f377d16eb07cd6605e3b48f200b8ac
Cristian Nunes <cristian.nv@hotmail.com>
2024-03-06 17:13:31 -0300

    att

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,48 +1,11 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
     static numeroDeContas = 0
-    agencia;
-    _cliente;
     
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor
-        }
-    }
-
-    get cliente(){
-        return this._cliente
-    }
-
-    _saldo = 0; // é privado por boa prática, mas tbm não é oficial
-    // #saldo = 0 => privado, mas ainda não foi aprovado
-
-    constructor(agencia, cliente) {
-        this.agencia = agencia
-        this._cliente = cliente
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia)
         ContaCorrente.numeroDeContas += 1
-    }
-
-    get saldo(){
-        return this._saldo
-    }
-
-    sacar(valor) {
-        if(this._saldo >= valor){
-            this._saldo -= valor
-            return valor
-        }
-    }
-
-    depositar(valor) {
-        if(valor > 0){
-            this._saldo += valor
-        }
-    }
-
-    transferir(valor, conta) {
-        const valorSacado = this.sacar(valor)
-        conta.depositar(valorSacado)
     }
 }
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import { Cliente } from "./Cliente.js"
+import { Conta } from "./Conta.js"
 import { ContaCorrente } from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js"
 
 const cliente1 = new Cliente('Ricardo', 11122233309)
 
 const cliente2 = new Cliente('Alice', 88822233309)
 
-const conta1 = new ContaCorrente(1001, cliente1)
+const conta1 = new ContaCorrente(cliente1, 1001)
 
-const conta2 = new ContaCorrente(102, cliente2)
+const conta2 = new ContaCorrente(cliente2, 102)
 
-console.log(ContaCorrente.numeroDeContas)
+const poupanca = new ContaPoupanca(50, cliente2, 102)
+
+conta1.depositar(200)
+conta2.depositar(400)
+
+console.log(poupanca, conta1, conta2)
