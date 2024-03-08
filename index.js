a373f374b2f38585f0b2c75751243a531ab80a61
Cristian Nunes <cristian.nv@hotmail.com>
2024-03-08 19:05:44 -0300

    att

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js"
-import { ContaPoupanca } from "./ContaPoupanca.js"
-import { ContaSalario } from "./ContaSalario.js"
+import { Gerente } from "./Funcionario/Gerente.js"
+import { Diretor } from "./Funcionario/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente('Ricardo', 11122233309)
+const diretor = new Diretor('Cristian', 10000, 12345678900)
+diretor.cadastrarSenha('1234')
+const gerente = new Gerente('Rafael', 5000, 32165498700)
 
-const cliente2 = new Cliente('Alice', 88822233309)
+const estaLogado = SistemaAutenticacao.login(diretor, '1234')
 
-const conta1 = new ContaCorrente(cliente1, 1001)
-
-const conta2 = new ContaCorrente(cliente2, 102)
-
-const poupanca = new ContaPoupanca(50, cliente2, 102)
-
-const salario = new ContaSalario(cliente2)
-
-conta1.depositar(200)
-conta2.depositar(400)
-
-console.log(poupanca, conta1, conta2, salario)
+console.log(estaLogado)
